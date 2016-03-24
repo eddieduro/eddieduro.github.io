5b914c943b3e6289ac35fe594595e48a012b5079
eddie <e.jamesduro@gmail.com>
2016-03-23 23:42:03 -0700

    add social icons to footer

--- build/js/app.js
+++ build/js/app.js
@@ -12,6 +12,38 @@ $(document).ready(function(){
       $hamburger.toggleClass("is-active");
       $("#navigation.reveal").addClass("hidden");
     }
+
+    (function() {
+
+    // FORM / CONTACT
+
+    var $contactForm = $('#contactForm');
+    // VALIDATE FORM
+    $contactForm.validate();
+    $contactForm.submit(function(e) {
+       if ($(this).valid())
+        e.preventDefault();
+        $.ajax({
+            url: '//formspree.io/myemail@mail.com',
+            method: 'POST',
+            data: $(this).serialize(),
+            dataType: 'json',
+            beforeSend: function() {
+                $contactForm.append('<div class="message message--loading">Sending message…</div>');
+            },
+            success: function(data) {
+                $contactForm.find('.alert--loading').hide();
+                $contactForm.append('<div class="message message--success">Message sent!</div>');
+            },
+            error: function(err) {
+                $contactForm.find('.alert--loading').hide();
+                $contactForm.append('<div class="message message--error">Ops, there was an error.</div>');
+            }
+        });
+       return false;
+        
+    });
+})();
   });
 });
 
